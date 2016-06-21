aa940a2db73a160708ee050ca71aed12509539d8
Raghav Mathur <raghav3501@gmail.com>
2016-06-21 19:51:03 +0530

    Finished Button 3

--- buttons.js
+++ buttons.js
@@ -17,10 +17,10 @@ $.fn.animateRotate = function(angle, duration, easing, complete) {
 
 var x = true;
 var y = true;
+var z = true;
 $(document).ready(function() {
   /*js button 1*/
   $('.container').click(function() {   
-
     if(x){
       $('.container').animateRotate(360, 900);
       $('#l2').animate({
@@ -34,8 +34,7 @@ $(document).ready(function() {
         width: '50px'
       }, 200);
       x = false;
-    }
-    else{
+    }else{
       $('#l2').animate({
         width: "80px"
       }, 150);
@@ -78,6 +77,66 @@ $(document).ready(function() {
       $("#bar1").animateRotate(0, 200);
       $("#bar3").animateRotate(0, 200);
       y=true;
-    }
+    };
+  });
+  /*end js button 2*/
+
+  /*js button 3*/
+
+  $(".br").click(function() {
+    if (z) {
+    $("#br2").animate({
+      width: "70%",
+      height: "10%",
+      marginTop: "-15%",
+      marginLeft:"13%" 
+
+    });
+
+    $("#br1").animate({
+      width: "30%",
+      height: "10%",
+      marginTop: "20%",
+      marginLeft: "10%"
+    });
+
+    $("#br3").animate({
+      width: "30%",
+      height: "10%",
+      marginTop: "-13%",
+      marginLeft: "10%"
+    });
+
+    $("#br1").animateRotate(-45, 200);
+    $("#br3").animateRotate(45, 200);
+
+    z=false;
+  }else{
+     $("#br2").animate({
+      width: "80%",
+      height: "17%",
+      marginTop: "-0",
+      marginLeft:"0%" 
+
+    });
+
+    $("#br1").animate({
+      width: "80%",
+      height: "17%",
+      marginTop: "0%",
+      marginLeft: "0%"
+    });
+
+    $("#br3").animate({
+      width: "80%",
+      height: "17%",
+      marginTop: "-0%",
+      marginLeft: "0%"
+    });
+
+    $("#br1").animateRotate(0, 200);
+    $("#br3").animateRotate(0, 200);
+    z=true;
+  }
   });
 });
